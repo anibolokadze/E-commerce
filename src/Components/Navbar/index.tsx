b32eb83302df76cf28ed71b41cc3e3e606e7d3a6
unknown <anibolokadze@gmail.com>
2023-06-19 11:07:00 -0700

    new navigation

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,46 +1,107 @@
-import globe from "../../assets/globe.svg";
+import { SetStateAction, useState } from "react";
+import "./Navbar.scss";
 import wishlist from "../../assets/wishlist.svg";
 import shop from "../../assets/shop.svg";
-import user from "../../assets/user.svg";
-import "./Navbar.scss";
 
-const Navbar = () => {
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState({});
+  const [activeCategory, setActiveCategory] = useState("");
+
+  const handleOptionChange = (
+    category: string,
+    event: {
+      target: { value: SetStateAction<string> };
+    }
+  ) => {
+    setSelectedOption((prevSelectedOption) => ({
+      ...prevSelectedOption,
+      [category]: event.target.value,
+    }));
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const toggleList = (category: SetStateAction<string>) => {
+    setActiveCategory(category === activeCategory ? "" : category);
+  };
+
+  const categories = [
+    {
+      name: "Men",
+      subcategories: ["Men", "New Arrivals", "Best Sellers", "Under $100"],
+    },
+    {
+      name: "Women",
+      subcategories: ["Women", "New Arrivals", "Best Sellers", "Under $100"],
+    },
+    {
+      name: "Jewelry",
+      subcategories: ["Jewelry", "New Arrivals", "Best Sellers", "Under $100"],
+    },
+  ];
+
   return (
     <nav>
-      <ul className="top-nav">
-        <li className="flex-column">
-          <img src={user} alt={user} />
-          Sign In
-        </li>
-        <li className="flex-column">
-          <img src={globe} alt={globe} />
-          English, usd
-        </li>
-      </ul>
+      <h1 className="logo">STORE</h1>
+      <div className={`menu ${isOpen ? "open" : ""}`}>
+        <button className="burger" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </button>
+        <ul className="menu-options">
+          {categories.map((category, index) => (
+            <li key={index}>
+              <div className="custom-select">
+                <button
+                  className={`select-button ${
+                    category.name === activeCategory ? "active" : ""
+                  }`}
+                  onClick={() => toggleList(category.name)}
+                >
+                  {selectedOption[category.name]
+                    ? selectedOption[category.name]
+                    : category.name}
+                </button>
+                {category.name === activeCategory && (
+                  <ul className="options">
+                    {category.subcategories.map((subcategory, index) => (
+                      <li
+                        key={index}
+                        onClick={() =>
+                          handleOptionChange(category.name, {
+                            target: { value: subcategory },
+                          })
+                        }
+                      >
+                        {subcategory}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
 
-      <ul className="main-nav">
-        <div className="name">
-          <h1>COZA</h1>
-          <span>STORE</span>
-        </div>
-
-        <div className="nav-links">
-          <li>Men</li>
-          <li>Women</li>
-          <li>Jewelery</li>
-        </div>
         <div className="right-nav">
-          <form action="/search" method="get">
-            <input type="text" name="search" placeholder="Search" />
+          <form>
+            <input
+              type="search"
+              id="search-input"
+              name="search"
+              placeholder="Search"
+            />
           </form>
-          <div className="images">
-            <img src={wishlist} alt={wishlist} className="wishlist" />
-            <img src={shop} alt={shop} />
-          </div>
+          <img src={wishlist} alt={wishlist} />
+          <img src={shop} alt={shop} />
         </div>
-      </ul>
+      </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Navigation;
