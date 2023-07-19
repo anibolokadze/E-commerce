c1dd0d5faece4bca1a959dd8a783fd5506ff0ca8
unknown <anibolokadze@gmail.com>
2023-07-19 16:07:47 -0700

    styles

--- src/pages/MenCategory/index.tsx
+++ src/pages/MenCategory/index.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { fetchMenCategory, MenCategoryData } from "../../api";
 import loading from "../../assets/loading.gif";
 import notFound from "../../assets/notfound.gif";
+import eye from "../../assets/icons8-eye-30.png";
+import "../Home/Components/PopularProducts/index.scss";
+import { Link } from "react-router-dom";
 
 const MenCategory = () => {
   const [menCategory, setMenCategory] = useState<MenCategoryData[]>([]);
@@ -37,10 +40,23 @@ const MenCategory = () => {
         <ul>
           {menCategory.map((product) => (
             <li key={product.title}>
-              <img src={product.image} alt={product.image} />
-              <p>{product.price}</p>
-              <p>{product.title}</p>
-              <p>{product.description}</p>
+              <Link
+                to={`/popular-products/${encodeURIComponent(product.title)}`}
+                className="product-link"
+              >
+                <div className="product-container">
+                  <div className="image-wrapper">
+                    <img src={product.image} alt={product.title} />
+                  </div>
+                  <div className="product-info">
+                    <p>{product.title}</p>
+                    <p>$ {product.price}</p>
+                  </div>
+                </div>
+                <button className="button-overlay">
+                  <img src={eye} alt={eye} /> Quick view
+                </button>
+              </Link>
             </li>
           ))}
         </ul>
